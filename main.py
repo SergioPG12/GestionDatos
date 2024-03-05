cca0213d869bdad864067964f4f0b771d046e622
juankisuelamartin <144780658+juankisuelamartin@users.noreply.github.com>
2024-03-05 09:26:52 +0100

    Add Start from last commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from datetime import datetime
+import time
 
 username = 'juankisuelamartin'
 token = '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'
@@ -19,18 +21,59 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
 # Conexión a MongoDB
 collCommits = connection["GestiondeDatos"]["Commits"]
 
 repos_url = 'https://api.github.com/repos/{}/{}/commits?page={}&per_page={}'
-user = 'brave'
-project = 'brave-core'
-url = repos_url.format(user, project, 1, 1)
-query = {'client_id': client_id, 'client_secret': client_secret}
-r = requests.get(url, params=query, headers=headers)
-commits_dict = r.json()
+commit_url = 'https://api.github.com/repos/{}/{}/commits/{}'
+user = 'sourcegraph'
+project = 'sourcegraph'
 
-for commit in commits_dict:
-    commit['projectId'] = project
-    print(str(commit))
-    collCommits.insert_one(commit)
+# Fecha de inicio
+since_date = datetime(2019, 1, 1)
+
+# Obtener el último commit SHA procesado
+last_commit_record = collCommits.find_one({}, sort=[('_id', -1)])
+last_commit_sha = last_commit_record['sha'] if last_commit_record else None
+
+# Formato de fecha para la API de GitHub
+since_str = since_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+page = 1
+while True:
+    url = repos_url.format(user, project, page, 100)
+    query = {'client_id': client_id, 'client_secret': client_secret, 'since': since_str}
+    r = requests.get(url, params=query, headers=headers)
+    if r.status_code == 403:
+        print("Rate limit exceeded. Sleeping for a minute...")
+        time.sleep(120)
+        continue
+    commits_dict = r.json()
+    if not commits_dict:  # Si la lista está vacía, hemos llegado al final
+        break
+    for commit in commits_dict:
+        commit['projectId'] = project
+        print(str(commit))
+        # Obtener información adicional del commit
+        commit_sha = commit['sha']
+        if commit_sha == last_commit_sha:
+            # Si encontramos el último commit procesado, terminamos
+            break
+        commit_info_url = commit_url.format(user, project, commit_sha)
+        r = requests.get(commit_info_url, headers=headers)
+        commit_info = r.json()
+        if 'files' in commit_info:
+            commit['files'] = commit_info['files']
+        else:
+            commit['files'] = []
+        if 'stats' in commit_info:
+            commit['stats'] = commit_info['stats']
+        else:
+            commit['stats'] = []
+        collCommits.insert_one(commit)
+    else:
+        # Si no se encontró el último commit procesado, continuamos con la siguiente página
+        page += 1
+        continue
+    break  # Terminamos el bucle si encontramos el último commit procesado
